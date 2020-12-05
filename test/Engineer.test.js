c6c684f92600116637b10cf428cc3ce3cad4cf01
JC <sanjothebay@gmail.com>
2020-12-05 14:35:39 -0600

    pass all test

--- test/Engineer.test.js
+++ test/Engineer.test.js
@@ -16,4 +16,4 @@ test("Can get GitHub username via getGithub()", () => {
   const testValue = "GitHubUser";
   const e = new Engineer("Foo", 1, "test@test.com", testValue);
   expect(e.getGithub()).toBe(testValue);
-});
+}); 
