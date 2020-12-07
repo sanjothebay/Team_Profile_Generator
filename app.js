f6d64aa041ba71247a4807c8b435b95b6c0b566e
JC <sanjothebay@gmail.com>
2020-12-06 22:14:10 -0600

    removed all noted to make it look clean but keptthe consol.logs

--- app.js
+++ app.js
@@ -4,45 +4,13 @@ const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
-
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
-
 const render = require("./lib/htmlRenderer");
-
-// Write code to use inquirer to gather information about the development team members,
-// use the inquire pacjage to get the qustions needed ...
-// and to create objects for each team member (using the correct classes as blueprints!)
-
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not. created one and place it as in the README file Sructure
-
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
-// create instances for the team member
-// const manager = new Manager ();
-// const engineer = new Engineer ();
-// const intern = new Intern ();
-
-
 let TeamMembersArray = [];
-// array of questions for user
-// const questions =
-// function to call the question prompts
+
+
+
 
 function ManagerQuestions() {
   inquirer
@@ -73,12 +41,9 @@ function ManagerQuestions() {
         message: "Which type of team member you like to add?",
         name: "typeOfTeamMember",
         choices: ["Engineer", "Intern", "None"],
-        /*if, if/else, and else statement to run function again if 
-    they choose to add a team member Else render the HTML (*/
       },
     ])
 
-    //function to initialize program/unction call to initialize program
     .then((response) => {
       console.log(response);
       const manager = new Manager (response.managersName, response.managersId, response.managersEmail, response.managersOfficeNumber,);
@@ -98,8 +63,6 @@ function ManagerQuestions() {
       
     });
 }
-// Engineer set of questions
-// function to call the question prompts
 
 function engineersQuestions() {
   inquirer
@@ -129,8 +92,6 @@ function engineersQuestions() {
         message: "Which type of team member you like to add?",
         name: "typeOfTeamMember",
         choices: ["Engineer", "Intern", "None"],
-        /*if, if/else, and else statement to run function again if 
-    they choose to add a team member Else render the HTML */
       },
     ])
     .then((response) => {
@@ -153,8 +114,6 @@ function engineersQuestions() {
     });
 }
 
-// Intern set of questions
-// function to call the question prompts
 function InternQuestions() {
   inquirer
     .prompt([
@@ -183,8 +142,6 @@ function InternQuestions() {
         message: "Which Type of Team Member would you like to add?",
         name: "typeOfTeamMember",
         choices: ["Engineer", "Intern", "None"],
-        /*if, if/else, and else statement to run function again if 
-    they choose to add a team member Else render the HTML */
       },
     ])
     .then((response) => {
@@ -206,5 +163,4 @@ function InternQuestions() {
         }
     });
 }
-
 ManagerQuestions();
